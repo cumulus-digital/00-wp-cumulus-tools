21e3eaea17ae9ca3d30aa8e34c266457c5f6cded
Daniel Vena <deaconvena+github@gmail.com>
2022-05-30 20:19:56 -0400

    add margins for query loop and related

--- build/block-filters.asset.php
+++ build/block-filters.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-html-entities'), 'version' => '8642bf920324099513c0');
+<?php return array('dependencies' => array('lodash', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-html-entities'), 'version' => 'ca2c00f48c66586470cb');
