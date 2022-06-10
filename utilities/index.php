3e8861534c5051cecc809b4987b95153427c2456
Daniel Vena <deaconvena@gmail.com>
2022-06-09 23:10:55 -0400

    turn on displaying advanced wpo stuff

--- utilities/index.php
+++ utilities/index.php
@@ -5,4 +5,5 @@ namespace CUMULUS\Gutenberg\Tools\Utilities;
 /*
  * Runs utility entries
  */
+require __DIR__ . '/wp-optimize.php';
 require __DIR__ . '/collapse-hierarchical/index.php';
---
+++ utilities/wp-optimize.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Enable showing advanced minify options
+ */
+if ( ! \defined( 'WP_OPTIMIZE_SHOW_MINIFY_ADVANCED' ) ) {
+	\define( 'WP_OPTIMIZE_SHOW_MINIFY_ADVANCED', true );
+}
