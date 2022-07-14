0cc42bbd0f6ed7ad0bb09114b1aded2b3a40df6f
Daniel Vena <deaconvena@gmail.com>
2022-07-14 11:54:59 -0400

    fix regexp and code style

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,55 +1,43 @@
 <?php
-/*
-* PHP-CS-Fixer project configuration
-*/
+// PHP-CS-Fixer project configuration
 $config = new PhpCsFixer\Config();
-require_once __DIR__ . '/vendor/cumulus-digital/wp-php-cs-fixer/loader.php';
+
+require_once __DIR__ . '/vendor/autoload.php';
+$RuleSet = new vena\WordPress\PhpCsFixer\WordPressRuleSet();
 
 // Load WP core classes/functions/constants for qualifying
 $wp_core      = \str_getcsv( \file_get_contents( __DIR__ . '/phpcs-tokens/tokens/wp-core.csv' ) );
 $NFI_includes = \array_filter( \array_merge(
-	['@compiler_optimized'],
-	['@internal'],
+	array( '@compiler_optimized' ),
+	array( '@internal' ),
 	$wp_core
 ) );
 
 return $config
-	->registerCustomFixers( [
-		new WeDevs\Fixer\SpaceInsideParenthesisFixer(),
-		new WeDevs\Fixer\BlankLineAfterClassOpeningFixer(),
-	] )
+	->registerCustomFixers( $RuleSet->getCustomFixers() )
 	->setRiskyAllowed( true )
 	->setIndent( "\t" )
 	->setRules( \array_merge(
-		WeDevs\Fixer\Fixer::rules(),
-		[
-			'@PSR2' => true,
-			// Each element of an array must be indented exactly once.
-			'array_indentation' => true,
+		$RuleSet->getRules(),
+		array(
+			'binary_operator_spaces' => array( 'default' => 'align_single_space_minimal' ),
 			// Replace non multibyte-safe functions with corresponding mb function.
 			'mb_str_functions' => true,
 			// Add leading `\` before constant invocation of internal constant to speed up resolving. Constant name match is case-sensitive, except for `null`, `false` and `true`.
 			'native_constant_invocation' => true,
 			// Add leading `\` before function invocation to speed up resolving.
-			'native_function_invocation' => ['strict' => false, 'include' => $NFI_includes],
+			'native_function_invocation' => array(
+				'strict'  => false,
+				'include' => $NFI_includes,
+			),
 			// Master language constructs shall be used instead of aliases.
 			'no_alias_language_construct_call' => true,
-			// PHP single-line arrays should not have trailing comma.
-			'no_trailing_comma_in_singleline_array' => true,
-			// Remove trailing whitespace at the end of blank lines.
-			'no_whitespace_in_blank_line' => true,
-			// Logical NOT operators (`!`) should have leading and trailing whitespaces.
-			'not_operator_with_space' => true,
-			// Standardize spaces around ternary operator.
-			'ternary_operator_spaces' => true,
-			// Multi-line arrays, arguments list and parameters list must have a trailing comma.
-			'trailing_comma_in_multiline' => true,
-			// Replace control structure alternative syntax to use braces.
-			'no_alternative_syntax' => false,
-		],
+		),
 	) )
 	->setFinder(
 		PhpCsFixer\Finder::create()
 			->exclude( 'vendor' )
+			->exclude( 'build/composer' )
 			->in( __DIR__ )
-	);
+	)
+;
--- block-filters/ajax-query-loop/filters.php
+++ block-filters/ajax-query-loop/filters.php
@@ -6,9 +6,7 @@ use function CUMULUS\Gutenberg\Tools\contains_block;
 
 \defined( 'ABSPATH' ) || exit( 'No direct access allowed.' );
 
-/*
- * Filter a query loop block to deal with ajax options
- */
+// Filter a query loop block to deal with ajax options
 \add_filter( 'render_block', function ( $content, $block ) {
 	if (
 		$block['blockName'] !== 'core/query' || ! isset( $block['attrs']['useAjax'] ) || ! $block['attrs']['useAjax']
@@ -19,7 +17,7 @@ use function CUMULUS\Gutenberg\Tools\contains_block;
 	$query_id      = $block['attrs']['queryId'];
 	$container_end = \mb_strpos( $content, '>' );
 
-	$paged = \absint( $_GET[ 'query-' . $query_id . '-page' ] ?? 1 );
+	$paged = \absint( $_GET['query-' . $query_id . '-page'] ?? 1 );
 
 	$content = \substr_replace(
 		$content,
@@ -36,30 +34,30 @@ use function CUMULUS\Gutenberg\Tools\contains_block;
 /**
  * WP_Query "where" filter to remove any post statuses that
  * are not "publish" since AJAX calls may include others.
+ *
+ * @param mixed $where
  */
 function filter_unpublished( $where ) {
+	global $wpdb;
 	\preg_match_all(
-		"/OR\s*wp_posts\.post_status\s*=\s*['\"][^'\"]+['\"]/",
+		'/OR\s*' . $wpdb->posts . '\.post_status\s*=\s*[\'\"][^\'\"]+[\'\"]/',
 		$where,
 		$statuses
 	);
 
-	$replace = [];
+	$replace = array();
 
 	if ( $statuses && \count( $statuses ) ) {
 		$statuses = $statuses[0];
 		$replace  = \array_filter( $statuses, function ( $status ) {
-			return \mb_strpos( $status, "'publish'" ) === false;
+			return \mb_strpos( $status, 'publish' ) === false;
 		} );
 	}
-	$where = \str_replace( $replace, '', $where );
 
-	return $where;
+	return \str_replace( $replace, '', $where );
 }
 
-/*
- * Handle rendering of new pages
- */
+// Handle rendering of new pages
 function query_pagination_render_more_query() {
 	$block = \json_decode( \stripslashes( $_REQUEST['block'] ), true );
 	$paged = ! empty( $_REQUEST['paged'] ) ? \intval( $_REQUEST['paged'] ) : 1;
@@ -85,9 +83,9 @@ function query_pagination_render_more_query() {
 		}
 		$_GET['query-' . $block['attrs']['queryId'] . '-page'] = $paged;
 
-		\add_filter( 'posts_where', __NAMESPACE__ . '\\filter_unpublished', 999999999, 1 );
+		\add_filter( 'posts_where', __NAMESPACE__ . '\\filter_unpublished', \PHP_INT_MAX, 1 );
 		echo \render_block( $block );
-		\remove_filter( 'posts_where', __NAMESPACE__ . '\\filter_unpublished', 999999999 );
+		\remove_filter( 'posts_where', __NAMESPACE__ . '\\filter_unpublished', \PHP_INT_MAX );
 	}
 
 	if ( $post ) {
@@ -95,25 +93,25 @@ function query_pagination_render_more_query() {
 	}
 	\wp_die();
 }
-\add_action( 'wp_ajax_query_render_more_pagination', __NAMESPACE__ . '\query_pagination_render_more_query' );
-\add_action( 'wp_ajax_nopriv_query_render_more_pagination', __NAMESPACE__ . '\query_pagination_render_more_query' );
+\add_action( 'wp_ajax_query_render_more_pagination', __NAMESPACE__ . '\\query_pagination_render_more_query' );
+\add_action( 'wp_ajax_nopriv_query_render_more_pagination', __NAMESPACE__ . '\\query_pagination_render_more_query' );
 
 \add_action( 'enqueue_block_assets', function () {
 	if ( contains_block( 'core/query' ) && ! \is_admin() ) {
 		\wp_enqueue_script(
 			'core-query-ajax-handler',
 			\CUMULUS\Gutenberg\Tools\BASEURL . 'block-filters/ajax-query-loop/handle_ajax.js',
-			['jquery'],
+			array( 'jquery' ),
 			false,
 			true
 		);
 		\wp_localize_script(
 			'core-query-ajax-handler',
 			'core_query_ajax_handler',
-			[
+			array(
 				'url'     => \admin_url( 'admin-ajax.php' ),
 				'post_id' => \get_the_ID(),
-			]
+			)
 		);
 		\wp_enqueue_style(
 			'core-query-ajax-handler',
