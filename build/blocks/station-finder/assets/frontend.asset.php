bb36adc6fda7b57b66689f93f316bd039980b84b
Daniel Vena <deaconvena+github@gmail.com>
2026-05-07 12:49:08 -0400

    didn't work, go back to basic api status message

--- build/blocks/station-finder/assets/frontend.asset.php
+++ build/blocks/station-finder/assets/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react-jsx-runtime'), 'version' => '4322583ee26791c6524d');
+<?php return array('dependencies' => array('lodash', 'react-jsx-runtime'), 'version' => '634e5adc12d31989b534');
